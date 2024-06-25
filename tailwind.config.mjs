da4f14f8f288da0e9faa19307ce55c45463e2a42
davidelng <davidelngyt@gmail.com>
2024-06-26 00:22:52 +0200

    aurora light mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'aurora-gradient': "radial-gradient(at 93% 45%, hsla(18,100%,84%,0.15) 0px, transparent 50%),radial-gradient(at 40% 20%, hsla(33,100%,96%,1) 0px, transparent 50%),radial-gradient(at 0% 100%, hsla(9,48%,83%,0.43) 0px, transparent 50%),url('noise.svg')"
+			},
+			backgroundColor: {
+				'aurora-base': "hsla(33,100%,96%,1)"
+			}
 		},
 	},
 	plugins: [],
